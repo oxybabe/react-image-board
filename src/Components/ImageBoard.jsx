986f59dcfaaf009bf4d3c4bd630597f3a3d3a9b8
Elia Partin <arenaselia@gmail.com>
2023-06-01 09:00:27 -0400

    added new pages

--- src/Components/ImageBoard.jsx
+++ src/Components/ImageBoard.jsx
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
+import ImageForm from './ImageForm';
+import ImageList from './ImageList';
+import { image } from './ImageList';
 
-function ImageBoard(){
+
+export default function ImageBoard(){
   const[image, setImage] = useState("")
 
     return(
       <div>
         <h1>Image Board</h1>
         <ImageForm imgURL={image} changeURL={setImage} />
-        <ImageList img={image}  />
+        {/* <ImageList img={image}  /> */}
       </div>
     )
 }
 
-export default ImageBoard
--- src/Components/ImageForm.jsx
+++ src/Components/ImageForm.jsx
@@ -1,27 +1,31 @@
- function ImageForm(props){
-    const[imageUrl, setImageURL] = useState("");
+import React, { useState } from 'react';
+
+export default function ImageForm(props) {
+    const[imageURL, setImageURL] = useState("");
     const[imageCaption, setImageCaption] = useState("");
-    console.log(props.imgURL)
-    console.log(props.changeURL)
+
+
+//  function ImageForm(props){
+//     const[imageUrl, setImageURL] = useState("");
+//     const[imageCaption, setImageCaption] = useState("");
+//     console.log(props.imgURL)
+//     console.log(props.changeURL)
 
     return(
-  
+    <>
       <form>
+        {/* <input type="url" name="" id="" />
         <input type="url" onChange={(e) => props.changeURL(e.target.value)} value={props.imgURL} />
-        
-        <input type="url" onChange={(e) => setImageUrl(e.target.value)} value={imageURL} />
+         */}
+        <input type="url" onChange={(e) => props.setImageUrl(e.target.value)} value={props.imageURL} placeholder='Image URL'/>
         <textarea type="text" name="caption"  placeholder='Image Caption'/>
-        <input
-        type="file"
-        id="upload-button"
-      />
+        
         <input type="button" value="" placeholder='Cancel'/>
         <input type="button"  placeholder="Add Image" />
         
       </form>
-  
+    </>
     );
    
 
 };
-export default ImageForm
